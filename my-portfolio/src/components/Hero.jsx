42f0d0d4eb88205863511ebc1ff923d09392f2bd
Anguru Baddalage Oshada Pramod Nandarathna <oshadapramod99@gmail.com>
2026-09-11 22:30:41 +0530

    Update portfolio website

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -291,8 +291,8 @@ function HeroComponent() {
                 <div className="hero-content">
                     <h2 className="hero-subtitle">Hi, I am</h2>
                     <h1 className="hero-title" ref={heroTitleRef}>Oshada Pramod</h1>
-                    <p className="hero-description">Aspiring DevOps / SRE Engineer
-                        <br />Computer Engineering Undergraduate
+                    <p className="hero-description"> Site Reliability Engineer
+                        <br />Computer Engineering Graduate
                     </p>
 
                     <div className="social-links">
